3889ad5eec85146bef284273db60fbd3646d8af5
Burak <burakbilgin2207@gmail.com>
2026-07-02 23:53:03 +0300

    chore: bump version to 1.0.5.2 and optimize release build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 36
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0.5.1"
+        versionName = "1.0.5.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
